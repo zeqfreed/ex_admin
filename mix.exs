87ef9fca7f78c2d5bc62cd16eaedf526515f70c5
Anton Kirsanov <a.kirsanov@fun-box.ru>
2017-05-19 11:29:06 +0400

    ex_queb version bump.

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule ExAdmin.Mixfile do
       {:exactor, "~> 2.2.0"},
       {:ex_doc, "~> 0.11", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
-      {:ex_queb, "~> 1.0", github: 'myasoedov/ex_queb', ref: 'f56bccbcdc771427bea1c25369d6b9be70a16363'},
+      {:ex_queb, "~> 1.0", github: 'myasoedov/ex_queb', ref: 'ba87b2af3d48aa97480702afd7fb2f60283d4ea2'},
       {:excoveralls, "~> 0.5", only: :test},
       {:gettext, "~> 0.11"},
       {:hound, "~> 1.0", only: :test}
